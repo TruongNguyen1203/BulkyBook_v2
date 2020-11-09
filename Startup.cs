969cab27dcea143f4d9a5679a62863c123b7df78
Nguyen Truong <nguyentruong.120300@gmail.com>
2020-11-09 09:26:56 +0700

    Authorization

--- Startup.cs
+++ Startup.cs
@@ -40,6 +40,23 @@ namespace BulkyBook
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+                options.LogoutPath = $"/Identity/Account/Logout";
+                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+            });
+            services.AddAuthentication().AddFacebook(options =>
+            {
+                options.AppId = "3538295166264482";
+                options.AppSecret = "086e5da46e92ecd3d8a745622a1d032b";
+            });
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                options.ClientId = "576353592147-7v2k2pakpjlsfvqvevl32gggoigbe7ll.apps.googleusercontent.com";
+                options.ClientSecret = "B35jRWqByUo1XzcjCJtZpIqD";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
